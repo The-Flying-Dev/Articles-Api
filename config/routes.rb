e7685f0bc092d3fdcd8d313143c769c183ab4b0f
Leandran <leandranm@outlook.com>
2021-10-17 16:38:46 +0200

    updated config.app.rb file and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  scope 'api' do
+    resources :articles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
